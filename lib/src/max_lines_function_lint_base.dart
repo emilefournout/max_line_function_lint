5ec285a9cf5635d199102d33897a86806b098f5e
emile_frn <emile.fournout@gmail.com>
2024-02-03 01:23:15 +0100

    add documentation

--- lib/src/max_lines_function_lint_base.dart
+++ lib/src/max_lines_function_lint_base.dart
@@ -2,6 +2,8 @@ import "package:analyzer/error/error.dart";
 import "package:analyzer/error/listener.dart";
 import "package:custom_lint_builder/custom_lint_builder.dart";
 
+/// This is the entrypoint of our custom linter
+
 MaxLinesFunctionLint createPlugin() => MaxLinesFunctionLint();
 
 /// This class is the one that will analyze Dart files and return lints
@@ -14,12 +16,15 @@ class MaxLinesFunctionLint extends PluginBase {
   }
 }
 
+/// This class contain the logic of the lint
 class MaxLinesFunctionRule extends DartLintRule {
   MaxLinesFunctionRule._(
     this._maxLine,
     LintCode _code,
   ) : super(code: _code);
 
+  /// Check if the user provide a custom limit
+  /// If not use _defaultMaxLine
   factory MaxLinesFunctionRule.init(CustomLintConfigs configs) {
     final maxLine =
         configs.rules[_name]?.json[_paramName] as int? ?? _defaultMaxLine;
@@ -54,6 +59,7 @@ class MaxLinesFunctionRule extends DartLintRule {
         final startLine =
             resolver.lineInfo.getLocation(startFileOffset).lineNumber;
         final endLine = resolver.lineInfo.getLocation(endFileOffset).lineNumber;
+
         final numberLine = endLine - startLine;
 
         if (numberLine > _maxLine) {
